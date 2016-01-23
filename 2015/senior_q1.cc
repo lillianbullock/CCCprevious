8e94295d36211f8b42cd37e4e058b589e37d6210
bullb2739 <lillianbullock@gmail.com>
2016-01-23 15:36:47 -0500

    Done

---
+++ 2015/senior_q1.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int num;
+	vector<int> vec;
+	
+	cin >> num;
+	
+	for (int c = 0; c < num; c++) {
+		int x;
+		cin >> x;
+		
+		if (x == 0) {
+			vec.pop_back();
+		}
+		
+		else {
+			vec.push_back(x);
+		}
+	}
+
+	int sum = 0;
+	while (vec.size() > 0) {
+		int x = vec.back();
+		sum = sum + x;
+		vec.pop_back();
+	}
+
+	cout << sum;
+
+	// your code goes here
+	return 0;
+}
